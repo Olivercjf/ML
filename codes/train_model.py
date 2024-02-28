91b1433d9ac1a24f464212f0ef0c56493f5cc2e0
Oliver Candia <oliver.candia5652@alumnos.udg.mx>
2024-02-28 01:17:26 -0600

    modifing file

--- codes/train_model.py
+++ codes/train_model.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # Load the model and define its configuration
-model = YOLO('/home/oliver18/Documents/ML/ML/codes/best.pt')
+model = YOLO('./best.pt')
 
 # Train the model
-results = model.val(data='/home/oliver18/Documents/ML/ML/data/butterflies_example.yaml', task='segment', epochs=10, imgsz=320, batch=8, save_json=False)
+results = model.val(data='../data/butterflies_example.yaml', task='segment', epochs=10, imgsz=320, batch=8, save_json=False)
 
 print(results.confusion_matrix.matrix)
